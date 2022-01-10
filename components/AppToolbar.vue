902feccbdd202cf80f12a2d0f873e14039d4418d
ChristopherAndradeXID <chris.andrade@xid.com.mx>
2022-01-09 20:36:02 -0600

    first version

--- components/AppToolbar.vue
+++ components/AppToolbar.vue
@@ -1,10 +1,12 @@
 <template>
   <v-app-bar app elevate-on-scroll elevation="1" color="white" fixed>
-    <img src="img/logo.png" height="100%" />
+    <img src="~/assets/img/logo.png" height="100%" />
     <v-spacer />
     <div class="d-none d-md-flex">
-      <v-btn text color="#071C87" to="/">Home</v-btn>
-      <v-btn text color="#071C87" to="/about-us">About US</v-btn>
+      <v-btn text color="#071C87" @click="localePathEx('index')">Home</v-btn>
+      <v-btn text color="#071C87" @click="localePathEx('about-us')"
+        >About US</v-btn
+      >
       <v-btn text color="#071C87">Services</v-btn>
       <v-btn text color="#071C87">Coverge</v-btn>
       <v-btn text color="#071C87">Contact US</v-btn>
@@ -17,8 +19,24 @@
         >
       </div>
       <div>
-        <v-btn depressed dark small color="#071C87">ES</v-btn>
-        <v-btn text outlined small color="#071C87">EN</v-btn>
+        <v-btn
+          @click="$i18n.setLocale('es')"
+          :outlined="$i18n.locale !== 'es'"
+          depressed
+          dark
+          small
+          color="#071C87"
+          >ES</v-btn
+        >
+        <v-btn
+          :outlined="$i18n.locale !== 'en'"
+          depressed
+          small
+          dark
+          color="#071C87"
+          @click="$i18n.setLocale('en')"
+          >EN</v-btn
+        >
       </div>
     </div>
     <v-app-bar-nav-icon class="d-flex d-md-none" color="#00126A" />
@@ -26,7 +44,13 @@
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    localePathEx(route) {
+      this.$router.push(this.localePath(route))
+    },
+  },
+}
 </script>
 
 <style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  
+  <div>
+    <p>{{ $t('welcome') }}</p>
+    <v-btn @click="$i18n.setLocale('es')">ES</v-btn>
+    <v-btn @click="$i18n.setLocale('en')">EN</v-btn>
+  </div>
 </template>
 
 <script>
 export default {
+  nuxtI18n: {
+    paths: {
+      en: '/home',
+      es: '/inicio'
+    }
+  },
   name: 'IndexPage',
 }
 </script>
